9db4353e98f106ce6b1993693b2250ad9bdf4e82
mmmdevlab <mmmdevlab@gmail.com>
2026-05-01 01:03:44 +0800

    feat: add task management with completion toggle

---
+++ backend/src/routes/task.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+// const router = express.Router();
+// // const taskController = require("../controllers/taskController");
+// const verifyToken = require("../middleware/verifyToken");
+
+// router.use(verifyToken);
+
+// router.post("/", taskController.createSupportRequest);
+// router.get("/", taskController.getMySupportRequests);
+// router.get("/:id", taskController.getSupportRequestById);
+// router.put("/:id", taskController.updateSupportRequest);
+// router.delete("/:id", taskController.deleteSupportRequest);
+
+// router.get("/assigned/requests", taskController.getAssignedSupportRequests);
+// router.patch("/:id/respond", taskController.respondToSupportRequest);
+// router.patch("/:id/complete", taskController.completeSupportRequest);
+
+// module.exports = router;
